cd686eef6f20f91971255804d923ddaf86169f26
felikslv <felikslv@163.com>
2022-05-10 15:15:59 +0800

    fix: header error

--- login.py
+++ login.py
@@ -46,7 +46,7 @@ def login_request(username, password) -> bool:
             'X-Requested-With': 'XMLHttpRequest',
         }
         if not is_login_as_pc:
-            headers['User-Agent'] = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Mobile Safari/537.36',
+            headers['User-Agent'] = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Mobile Safari/537.36'
         try:
             response = session.post(requesr_url, data=data, headers=headers)
             response.encoding = response.apparent_encoding
